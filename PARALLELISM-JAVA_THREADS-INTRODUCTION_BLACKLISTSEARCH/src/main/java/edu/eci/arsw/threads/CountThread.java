f6b19e15f08b225179706f926dd9d45ca506fe14
NicolasCastro9 <tankiandown@gmail.com>
2023-08-19 21:36:12 -0500

    Punto 1

--- PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH/src/main/java/edu/eci/arsw/threads/CountThread.java
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH/src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,34 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread{
+
+    private int numA;
+
+    private int numB;
+
+ 
+
+    public CountThread(int numA, int numB, String name){
+
+        super(name);
+
+        this.numA = numA;
+
+        this.numB = numB;
+
+    }
+
+   
+
+     public void run(){
+
+        for (int i = numA; i < numB ; i++)
+
+        System.out.println(i + " " + getName());
+
+        System.out.println("Termina thread " + getName());
+
+    }
+
 }
--- PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH/src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH/src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,31 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        Thread hilo1 = new Thread(new CountThread(0,100,"hilo1"));
+
+ 
+
+        Thread hilo2 = new Thread(new CountThread(99,200,"hilo2"));
+
+ 
+
+        Thread hilo3 = new Thread(new CountThread(199,300,"hilo3"));
+
+ 
+
+        hilo1.start();
+
+        hilo2.start();
+
+        hilo3.start();
+
+ 
+
+        hilo1.run();
+
+        hilo2.run();
+
+        hilo3.run();
     }
     
 }
